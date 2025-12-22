2ebd58be629417d0ece6be3dba3a05c539a88802
arjunpjaiswal <arjunphj@gmail.com>
2025-12-23 00:56:28 +0530

    added routing

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@
 import './index.css'
 import App from './App.jsx'
 import ReactDOM from 'react-dom/client'
-
+import { BrowserRouter } from 'react-router-dom'
 ReactDOM.createRoot(document.getElementById('root')).render(
-  
-    <App />
+  <BrowserRouter>
+     <App />
+  </BrowserRouter>
+   
   
 )
---
+++ src/routes/CustomRoutes.jsx
@@ -0,0 +1,12 @@
+import { Routes, Route } from 'react-router-dom'
+import PokemonDetails from '../Components/PokemonDetails/PokemonDetails'
+import Pokedex from '../Components/Pokedex/Pokedex';
+function CustomRoutes() {
+    return (
+        <Routes>
+            <Route path="/" element={<Pokedex />}></Route>
+            <Route path="/pokemon/:id" element={<PokemonDetails />} />
+        </Routes>
+    );
+}
+export default CustomRoutes
